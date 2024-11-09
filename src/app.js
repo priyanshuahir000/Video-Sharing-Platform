2f4da63c8fe469d7c6f0f783624293eefa300c61
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-11-09 19:14:11 +0530

    add asynhandler utility file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-export {app} ;
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "60kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+export { app };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+/**
+ * A higher-order function that wraps an asynchronous route handler
+ * and catches any errors that occur, passing them to the next middleware.
+ *
+ * @param {Function} fn - The asynchronous route handler to wrap.
+ * @returns {Function} A new route handler that catches errors and passes them to the next middleware.
+ */
+
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export { asyncHandler };
