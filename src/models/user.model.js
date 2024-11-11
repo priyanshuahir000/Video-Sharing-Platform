18442ade766a206e18b684735413e26da24a85a6
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-11-11 21:50:29 +0530

    add JWT token generation and bcrypt password hashing to User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,53 +1,96 @@
 import mongoose, { Schema } from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true, // this will automatically trim the whitespaces from the username
-    index: true, // this will be used to search the user by username
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
-    index: true,
-  },
-  avatar: {
-    type: String, // Cloudinary URL
-    required: true,
-  },
-  coverImage: {
-    type: String, // Cloudinary URL
-    required: true,
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Video",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true, // this will automatically trim the whitespaces from the username
+      index: true, // this will be used to search the user by username
     },
-  ],
-  password: {
-    type: String,
-    required: [true, "Password is required"],
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  refreshToken: {
-    type: String,
-  },
-}, 
-{
+  {
     timestamps: true,
-}
+  }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//We can use async function but usually jwt process is fast so we can use simple function
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
 
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -42,6 +43,7 @@ const videoSchema = new Schema(
   }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
 // in mongodb the table will be name as videos ( in plural form) and the schema will be videoSchema
