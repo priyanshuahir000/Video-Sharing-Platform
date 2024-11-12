4c10b0c828b68769ca53787206ff607389c1cf39
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-11-12 14:37:05 +0530

    Add Cloudinary upload functionality

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //file upload on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded on cloudinary
+    console.log("File has been uploaded on cloudinary ", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the local saved temporary file as the upload operation get failed as what we will do with the currupted file
+    return null;
+  }
+};
+
+cloudinary.v2.uploader.upload("", { public_id: "" }, function (error, result) {
+  console.log(result);
+});
